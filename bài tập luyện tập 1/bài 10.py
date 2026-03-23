924436c627e6bbc56a5041a9d59f6e9688c158b9
Quach Van Vu <nanvu1234567@gmail.com>
2026-03-23 17:59:26 +0700

    Delete bài tập luyện tập 1 directory

--- bài tập luyện tập 1/bài 10.py
+++
@@ -1,9 +0,0 @@
-
-ma_sp = input("Nhập mã sản phẩm: ")
-ten = input("Nhập tên sản phẩm: ")
-gia = float(input("Nhập giá: "))
-
-with open("product.txt", "a", encoding="utf-8") as f:
-    f.write(f"{ma_sp};{ten};{gia}\n")
-
-print("Đã thêm sản phẩm vào tệp.")
--- bài tập luyện tập 1/bài 4.py
+++
@@ -1,5 +0,0 @@
-def in_1_100():
-    for i in range(1,101):
-        if i % 3 == 1:
-            print(i)
-in_1_100()
